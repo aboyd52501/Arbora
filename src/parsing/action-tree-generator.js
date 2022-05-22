2e58a3420681728a1771052f3236936c90bd9e51
aboyd52501 <aboyd52501@gmail.com>
2022-05-22 10:33:31 -0500

    Added documentation for action tree generator function

--- src/parsing/action-tree-generator.js
+++ src/parsing/action-tree-generator.js
@@ -4,6 +4,7 @@ const { generateTypeCastedArboraTree } = require('./ast-type-caster.js');
 const { IdentifierType } = require('../std-library/datatypes.js');
 
 // [[1, 2, add], 3, mul] -> mul[add[1, 2], 3]
+// Action trees represent a ready-to-run program that has not been passed to the interpreter yet.
 function generateActionTree(typedAST) {
     
     const last = typedAST.pop();
