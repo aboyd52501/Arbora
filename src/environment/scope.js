5457af110d9e1a56cbdb01dac6ae8558d82a090b
aboyd52501 <aboyd52501@gmail.com>
2022-05-22 13:33:05 -0500

    Added JSDoc comments to scope.js

--- src/environment/scope.js
+++ src/environment/scope.js
@@ -1,12 +1,11 @@
-/*
-
-    Scopes hold variables (including functions) and other scopes.
-    Scope is a tree of scopes. Every scope knows about its parent scope.
-    Scopes are used to resolve variable references.
-        If the immediate scope doesn't have a variable, it will recursively check its parent scope(s).
-
+/**
+ * Scopes hold variables (including functions).
+ * Scopes hold only refernce to their parent scope. Child scopes are not present within the parent.
+ * Scopes are used to resolve variable references.
+ *  If the immediate scope doesn't have a variable, it will recursively check its parent scope(s).
+ * @param {Scope} parent - The parent scope of this scope.
+ * @param {object} contents - The scope's contents.
 */
-
 class Scope {
 
     constructor(parent, contents, reference) {
@@ -18,9 +17,6 @@ class Scope {
         if(!(contents instanceof Object))
             throw new Error("Contents must be an object: " + contents);
         this.contents = contents;
-
-        // This lets us give the scope a unique key within the parent scope's table.
-        this.reference = reference || crypto.randomUUID();
 
     }
 
@@ -49,9 +45,12 @@ class Scope {
 
 const { BuiltInFunction, builtinFxns } = require('../std-library/builtinfxns.js');
 
-// Global Scopes are the top-level scopes.
-// They are used to hold immutable global variables and functions.
-// Every program must be a scope within a global scope.
+/**
+ *  Global Scopes are the top-level scopes.
+ * They are used to hold immutable global variables and functions.
+ * Every program must be a scope within a global scope.
+ * @param {object} contents - The global scope's contents.
+ */
 class GlobalScope extends Scope {
 
     constructor() {
